34393c482402a8313d7017a3f6e63f02ff982ddf
kubovotny <jn.novotnyjakub04@gmail.com>
2026-03-24 12:51:27 +0100

    Started with sentiment analysis

--- source/sentiment/sentiment.py
+++ source/sentiment/sentiment.py
@@ -1,19 +1,10 @@
+from classifier import get_sentiment
 import pandas as pd
 
-import nltk
+STATEMENTS_DIR = "~/Documents/School/bachelor-thesis/source/statements"
+POSTFIX = "_" "2022u"
 
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+label_chunks = pd.read_csv(f"{STATEMENTS_DIR}/labeled_chunks{POSTFIX}.csv")
 
-from nltk.corpus import stopwords
-
-from nltk.tokenize import word_tokenize
-
-from nltk.stem import WordNetLemmatizer
-
-# nltk.download('all')
-
-df = pd.read_csv('../gathering/data_raw1.csv', sep='|')
-
-print(df.tail())
-
-if __name__ == '__main__':...
+sample = label_chunks.iloc[:,:10]
+print(sample)
